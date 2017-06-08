74c576ea9baf56167fed600fdffacd42c6115e3b
Ashley Dillinger <ashleyd@AshleyD-MacBook.local>
2017-06-08 10:41:48 -0700

    header

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import Games from './games'
+import Header from './header';
 
 
 let Main = ({ dispatch }) => (
   <div>
+  	<Header />
     Hello Ashley!
     <Games />
   </div>  
